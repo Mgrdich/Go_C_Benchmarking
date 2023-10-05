533fadf2508aed86475f73c8a4ff43d6b774dbb3
Mgrdich <mgotm13@gmail.com>
2023-10-05 13:43:05 +0400

    minor change

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 /*
-#include <stdbool.h>
-bool IsPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    if (num <= 3) {
-        return true;
-    }
-    if (num % 2 == 0 || num % 3 == 0) {
-        return false;
-    }
-
-    int i = 5;
-    while (i * i <= num) {
-        if (num % i == 0 || num % (i + 2) == 0) {
-            return false;
-        }
-        i += 6;
-    }
-
-    return true;
+int GetM() {
+    return 1;
 }
 */
 import "C"
 import "fmt"
 
 func main() {
-	fmt.Println(C.IsPrime(69))
+	result := C.GetM()
+
+	fmt.Printf("Result: %d\n", result)
 }
