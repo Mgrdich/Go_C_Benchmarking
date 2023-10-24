3ca6a2e807eafb0afd21c2ee964155a3461cdaf3
Mgrdich <mgotm13@gmail.com>
2023-10-24 09:57:47 +0400

    minor modifications in the benchmarking scripts

--- benchmarking.sh
+++ benchmarking.sh
@@ -8,6 +8,12 @@ fi
 command_to_run="$1"
 output_file="$2"
 exec_file_name="$3"
+leak_exist=false
+
+if command -v leaks &> /dev/null; then
+  leak_exist=true
+fi
+
 
 
 # Clear the previous contents of the output file
@@ -20,7 +26,17 @@ for i in {1..10}; do
               echo "------------------Run $i---------------" >> "$output_file"
 
               if [ -n "$3" ]; then
+
+                    echo "Size of the binary" >>  "$output_file"
                     du -h "$exec_file_name" >> "$output_file"
+
+                if [ "$leak_exist" = true ]; then
+                  {
+                  echo "--- Leaks Start ---" ;
+                  leaks --atExit -- "./$exec_file_name";
+                  echo "--- Leaks End ---" ;
+                  }>> "$output_file"
+                fi
               fi
 
               eval "time $command_to_run"
